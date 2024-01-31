96cf4bcf2859c6c17de5dd780b48128fcea16d55
HugoLabedade <hugo.labedade@ynov.com>
2024-01-31 10:36:44 +0100

    SVD + mise en page du front

--- back.py
+++ back.py
@@ -1,19 +1,84 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+#from main_svd import print_similar_games
+import numpy as np
+import pandas as pd
+import time
+import os
+from dotenv import load_dotenv
+np.seterr(divide='ignore', invalid='ignore')
 app = FastAPI()
+load_dotenv()
+
+
+df = pd.read_csv("data/Dataset.csv", usecols=["UserID", "Game", "Score", "GameID"])
+game_data = pd.read_csv("data/Dataset.csv", usecols=["GameID", "Game", "Categories", "About the game", "Genres"])
+game_data = game_data.rename(columns={"About the game": "About_the_game"})
+game_data = game_data.drop_duplicates()
+game_data = game_data.reset_index(drop=True)
+
+ratings_mat = np.ndarray(shape=(np.max(df.GameID.values), np.max(df.UserID.values)))
+
+ratings_mat[df.GameID.values-1, df.UserID.values-1] = df.Score.values
+
+# normalisation de la matrice : on soustrait la moyenne
+normalised_mat = ratings_mat - np.asarray([(np.mean(ratings_mat, 1))]).T
+# ultérieure normalisation et transposition pour passer à la matrice A "classique"
+A = normalised_mat.T / np.sqrt(ratings_mat.shape[0] - 1)
+
+envu = os.environ["U"]
+envs = os.environ["S"]
+envVh = os.environ["Vh"]
+
+if envu == 0 or envs == 0 or envVh == 0:
+    U, S, Vh = np.linalg.svd(A, full_matrices=True)
+
+# Mettre à jour les valeurs
+os.environ["U"] = "1"
+os.environ["S"] = "1"
+os.environ["Vh"] = "1"
+
+# Écrire les modifications dans le fichier .env
+with open(".env", "w") as f:
+    for key, value in os.environ.items():
+        f.write(f"{key}={value}\n")
+
+def top_cosine_similarity(data, game, top_n=5):
+    index = game_data[game_data.Game == game].GameID.values[0] # game id starts from 1 in the dataset
+    game_row = data[index, :]
+    magnitude = np.sqrt(np.einsum('ij, ij -> i', data, data))
+    similarity = np.dot(game_row, data.T) / (magnitude[index] * magnitude)
+    sort_indexes = np.argsort(-similarity)
+    return sort_indexes[1:top_n+1]
+
+# fonction pour imprimer les titres des top 10 games les plus similaires à un jeu donné
+
+def print_similar_games(game_data, game, V):
+    
+    sliced = V.T[:, :25] # utilisation seulement des K features latentes les plus importantes
+    top_indexes = top_cosine_similarity(sliced, game, 5)
+    jeu_reco = {"Jeux": [], "Description": [], "Genre": []}
+    for id in top_indexes + 1:
+        jeu_reco["Jeux"].append(game_data[game_data.GameID == id].Game.values[0])
+        jeu_reco["Description"].append(game_data[game_data.GameID == id].About_the_game.values[0])
+        jeu_reco["Genre"].append(game_data[game_data.GameID == id].Genres.values[0])
+    return jeu_reco
+
 
 # Modèle factice pour la démonstration
 # Ici, vous utiliseriez votre propre modèle de machine learning
 def run_machine_learning_model(prompt):
+    global Vh, jeu_reco
     # Placeholder - Exécutez votre modèle sur le prompt
-    result = f"Le modèle a reçu le prompt : '{prompt}' et a retourné un résultat."
+    liste_jeux = print_similar_games(game_data, prompt, Vh)
+    result = pd.DataFrame({"Jeux": liste_jeux["Jeux"], "Description": liste_jeux["Description"], "Genres": liste_jeux["Genre"]})
     return result
+     
 
 class PromptRequest(BaseModel):
     prompt: str
 
 @app.post("/run_model/")
 async def run_model(prompt_request: PromptRequest):
-    result = run_machine_learning_model(prompt_request.prompt)
-    return {"result": result}
+        result = run_machine_learning_model(prompt_request.prompt)
+        return {"result": result}
--- front.py
+++ front.py
@@ -1,20 +1,34 @@
 import requests
 import streamlit as st
+from streamlit_searchbox import st_searchbox
+import pandas as pd
+import wikipedia
+
+
 
 # URL de l'API FastAPI
 API_URL = "http://localhost:8000/run_model/"  # Mettez à jour avec votre URL si nécessaire
 
+game_data = pd.read_csv("data/Dataset.csv", usecols=["Game"])
+game_data = game_data.drop_duplicates()
+game_data = game_data.reset_index(drop=True)
+
 st.title("Interface pour le modèle de machine learning")
 
-# Saisie du prompt par l'utilisateur
-user_input = st.text_area("Entrez votre prompt :")
+def search(searchterm: str) -> list[tuple[str, any]]:
+    liste = []
+    for i in range(len(game_data[game_data["Game"].str.contains(searchterm, case=False)].values)):
+        liste.append(str(game_data[game_data["Game"].str.contains(searchterm, case=False)].values[i])[2:-2])
+    return liste
+ 
+resultat = st_searchbox(search)
 
 if st.button("Exécuter le modèle"):
     # Appel à l'API FastAPI avec le prompt saisi par l'utilisateur
-    response = requests.post(API_URL, json={"prompt": user_input})
+    response = requests.post(API_URL, json={"prompt": resultat})
 
     if response.status_code == 200:
         result = response.json()["result"]
-        st.success(f"Résultat du modèle : {result}")
+        st.dataframe(result, hide_index=True, width=None)
     else:
         st.error("Une erreur s'est produite lors de l'exécution du modèle.")
