0e629cfcd1a6e777efdbfb2994710f67905ffabe
HugoLabedade <hugo.labedade@ynov.com>
2024-01-31 15:18:41 +0100

    ajout des images sur la page

--- back.py
+++ back.py
@@ -8,8 +8,8 @@ app = FastAPI()
 
 # Ouverture des csv
 df = pd.read_csv("data/Dataset.csv", usecols=["UserID", "Game", "Score", "GameID"])
-game_data = pd.read_csv("data/Dataset.csv", usecols=["GameID", "Game", "Categories", "About the game", "Genres"])
-game_data = game_data.rename(columns={"About the game": "About_the_game"})
+game_data = pd.read_csv("data/Dataset.csv", usecols=["GameID", "Game", "Categories", "About the game", "Genres", "Header image"])
+game_data = game_data.rename(columns={"About the game": "About_the_game", "Header image": "Header_image"})
 game_data = game_data.drop_duplicates()
 game_data = game_data.reset_index(drop=True)
 
@@ -36,11 +36,12 @@ def print_similar_games(game_data, game, V):
     
     sliced = V.T[:, :25] # utilisation seulement des K features latentes les plus importantes
     top_indexes = top_cosine_similarity(sliced, game, 5)
-    jeu_reco = {"Jeux": [], "Description": [], "Genre": []}
+    jeu_reco = {"Jeux": [], "Description": [], "Genre": [], "Image": []}
     for id in top_indexes + 1:
         jeu_reco["Jeux"].append(game_data[game_data.GameID == id].Game.values[0])
         jeu_reco["Description"].append(game_data[game_data.GameID == id].About_the_game.values[0])
         jeu_reco["Genre"].append(game_data[game_data.GameID == id].Genres.values[0])
+        jeu_reco["Image"].append(game_data[game_data.GameID == id].Header_image.values[0])
     return jeu_reco
 
 # Modèle factice pour la démonstration
@@ -49,7 +50,7 @@ def run_machine_learning_model(prompt):
     global Vh
     # Placeholder - Exécutez votre modèle sur le prompt
     liste_jeux = print_similar_games(game_data, prompt, Vh)
-    result = pd.DataFrame({"Jeux": liste_jeux["Jeux"], "Description": liste_jeux["Description"], "Genres": liste_jeux["Genre"]})
+    result = pd.DataFrame({"Jeux": liste_jeux["Jeux"], "Description": liste_jeux["Description"], "Genres": liste_jeux["Genre"], "Image": liste_jeux["Image"]})
     return result
 
 class PromptRequest(BaseModel):
--- front.py
+++ front.py
@@ -2,8 +2,6 @@ import requests
 import streamlit as st
 from streamlit_searchbox import st_searchbox
 import pandas as pd
-import wikipedia
-
 
 
 # URL de l'API FastAPI
@@ -15,13 +13,18 @@ game_data = game_data.reset_index(drop=True)
 
 st.title("Interface pour le modèle de machine learning")
 
+
 def search(searchterm: str) -> list[tuple[str, any]]:
     liste = []
     for i in range(len(game_data[game_data["Game"].str.contains(searchterm, case=False)].values)):
         liste.append(str(game_data[game_data["Game"].str.contains(searchterm, case=False)].values[i])[2:-2])
     return liste
+
+def render_image(url):
+    return f'<img src="{url}" width="150">'
  
 resultat = st_searchbox(search)
+
 
 if st.button("Exécuter le modèle"):
     # Appel à l'API FastAPI avec le prompt saisi par l'utilisateur
@@ -29,6 +32,30 @@ if st.button("Exécuter le modèle"):
 
     if response.status_code == 200:
         result = response.json()["result"]
-        st.dataframe(result, hide_index=True, width=None)
+        st.data_editor(
+            result,
+            column_config={
+                "Image": st.column_config.ImageColumn(
+                    "Image", help="Streamlit app preview screenshots"
+                )
+            },
+            hide_index=True,
+        )
+    
+    col1, mid, col2, mid2, col3, mid3, col4, mid4, col5 = st.columns([20,1,20,1,20,1,20,1,20])
+    with col1:
+        for image in result["Image"]:
+            st.image(result["Image"][image])
+    with col2:
+        st.image(result["Image"])
+    with col3:
+        st.image(result["Image"])
+    with col4:
+        st.image(result["Image"])
+    with col5:
+        st.image(result["Image"])
+    
+    for image in result["Image"]:
+        st.image(result["Image"][image], caption=result["Jeux"][image], width=70)
     else:
         st.error("Une erreur s'est produite lors de l'exécution du modèle.")
